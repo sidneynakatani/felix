62e645cdcbdfecdbe4cef889829dd5123c201e4a
sidney <sidney.nakatani@hotmail.com>
2017-04-13 01:59:57 -0300

    adjust

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,21 +1,21 @@
-package com.example;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
-
-	@RequestMapping("/")
-    	@ResponseBody
-    	String home() {
-      	      return "Hello World!";
-   	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
