7aa276464e628d30baaab03729035365636c9014
Sidney <sidney.nakatani@hotmail.com>
2021-01-10 17:08:38 -0300

    fix pom

--- java/lab/Application.java
+++ java/lab/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-//@ConfigurationProperties("app")
+@ConfigurationProperties("app")
 @SpringBootApplication
 public class Application {
 
